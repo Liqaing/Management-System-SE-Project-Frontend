d6a1be36064933be25273c49145d6391e999dc60
Huot Rithy <huotrithy01@gmail.com>
2024-10-20 12:34:11 +0700

    fix import path for ui component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import "./App.css";
 import { Button } from "./components/ui/button";
+import { Label } from "./components/ui/label";
 
 function App() {
   return (
     <>
+      <div>Heeelloo</div>
       <div>Hello</div>
+      <Label>Hello</Label>
       <Button>Hello</Button>
     </>
   );
