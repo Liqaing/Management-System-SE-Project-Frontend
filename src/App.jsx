2e51a65bf98cb74fb5e9c2a30e57118240e38a15
Rithy <huotrithy01@gmail.com>
2024-10-24 20:12:40 +0700

    move check token to app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
 import { RouterProvider } from "react-router-dom";
 import router from "./utils/routeHandler/routes";
+import { useContext } from "react";
+import { AppContext } from "./utils/context";
 
 const App = () => {
+  const { user, setUser } = useContext(AppContext);
+
+  useEffect(() => {
+    axios
+      .post("/api/auth/token")
+      .then((res) => {
+        const data = res.data;
+        const userData = {
+          isLogin: data.data.isLogin,
+          username: data.data.username,
+          role: data.data.role,
+        };
+        setUser(userData);
+      })
+      .catch((err) => {
+        setUser(null);
+      });
+  }, []);
   return <RouterProvider router={router} />;
 };
 
--- src/utils/context.jsx
+++ src/utils/context.jsx
@@ -5,22 +5,6 @@ const AppContext = createContext();
 
 const AppProvider = ({ children }) => {
   const [user, setUser] = useState(null);
-  useEffect(() => {
-    axios
-      .post("/api/auth/token")
-      .then((res) => {
-        const data = res.data;
-        const userData = {
-          isLogin: data.data.isLogin,
-          username: data.data.username,
-          role: data.data.role,
-        };
-        setUser(userData);
-      })
-      .catch((err) => {
-        setUser(null);
-      });
-  }, []);
 
   return (
     <AppContext.Provider value={{ user, setUser }}>
