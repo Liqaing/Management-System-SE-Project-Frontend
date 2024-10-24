7766024cf810cfcf486bef7bae81ef7bc7306b69
Rithy <huotrithy01@gmail.com>
2024-10-24 19:59:37 +0700

    make app as entry point of app

--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,13 @@ import { ThemeProvider } from "@mui/material";
 import theme from "./theme.js";
 import router from "./utils/routeHandler/routes.jsx";
 import { AppProvider } from "./utils/context.jsx";
+import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AppProvider>
       <ThemeProvider theme={theme}>
-        <RouterProvider router={router} />
+        <App />
       </ThemeProvider>
     </AppProvider>
   </StrictMode>
