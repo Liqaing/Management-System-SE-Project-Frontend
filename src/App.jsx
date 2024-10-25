2a00349b5f61d998969116be926ea6321be712e7
Rithy <huotrithy01@gmail.com>
2024-10-25 19:56:36 +0700

    merge with soda-dev

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import { RouterProvider } from "react-router-dom";
-import router from "./utils/routeHandler/routes";
-import { useContext, useEffect } from "react";
-import { AppContext } from "./utils/context.jsx";
-import axios from "axios";
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import FrontendLayout from "./component/layout/FrontendLayout";
+import HomePage from "./page/home/HomePage";
+import DashboardLayout from "./component/layout/DashboardLayout";
+import HomePageDash from "./page-dash/home/HomePageDash";
+import NotFoundPage from "./page/error/404";
+import LayoutAuth from "./component/layout/LayoutAuth";
+import LoginPage from "./auth/LoginPage";
+import RegisterPage from "./auth/RegisterPage";
 
 const App = () => {
-  const { setUser } = useContext(AppContext);
-
   useEffect(() => {
     axios
       .post("/api/auth/token")
@@ -23,7 +26,32 @@ const App = () => {
         setUser(null);
       });
   }, [setUser]);
-  return <RouterProvider router={router} />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* frontend route */}
+        <Route path="/" element={<FrontendLayout />}>
+          <Route path="" element={<HomePage />}></Route>
+          <Route path="home" element={<HomePage />}></Route>
+          <Route path="*" element={<NotFoundPage />}></Route>
+        </Route>
+
+        {/* backend route */}
+        <Route path="/dashboard" element={<DashboardLayout />}>
+          <Route path="" element={<HomePageDash />}></Route>
+          <Route path="home" element={<HomePageDash />}></Route>
+          <Route path="*" element={<NotFoundPage />}></Route>
+        </Route>
+
+        {/* Login Route */}
+        <Route path="/dashboard" element={<LayoutAuth />}>
+          <Route path="login" element={<LoginPage />}></Route>
+          <Route path="register" element={<RegisterPage />}></Route>
+          <Route path="*" element={<NotFoundPage />}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
