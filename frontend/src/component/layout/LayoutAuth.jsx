49c8f2521fbce18dd7e4b85622174c8412dd2ef5
soda.kroes <sodait25@gmail.com>
2024-10-26 12:57:44 +0700

    manage layout and route

--- frontend/src/component/layout/LayoutAuth.jsx
+++ frontend/src/component/layout/LayoutAuth.jsx
@@ -1,13 +1,17 @@
 
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { Header } from 'antd/es/layout/layout'
+import React, { Component } from "react";
+import { Link, Outlet } from "react-router-dom";
+import {
+  Menu,
+  Button,
+} from "antd";
 
 function LayoutAuth() {
   return (
     <>
         {/* <div>LayoutAuth</div> */}
-
-        {/* For Design Layout Auth */}
+        
         <Outlet />
     </>
   )
