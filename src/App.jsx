c2a2866af0476666dbb7b9aeed47f54efd2884dd
soda.kroes <sodait25@gmail.com>
2024-11-20 09:07:02 +0700

    config route

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,13 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import axios from "axios";
+import { AppContext } from "./utils/context";
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+  Navigate,
+  Outlet,
+} from "react-router-dom";
 import FrontendLayout from "./component/layout/FrontendLayout.jsx";
 import HomePage from "./page/home/HomePage.jsx";
 import NotFoundPage from "./page/error/404.jsx";
@@ -14,31 +23,61 @@ import EmployeePageDash from "./page-dash/user/EmployeePageDash.jsx";
 import CustomerPageDash from "./page-dash/user/CustomerPageDash.jsx";
 import POS from "./page-dash/pos/POS.jsx";
 
+
+const ProtectedRoute = ({ user }) => {
+  if (user === null) return <Navigate to="/auth/signin" />;
+  return <Outlet />;
+};
+
 const App = () => {
+  const { user, setUser } = useContext(AppContext);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios
+      .post("/api/auth/token")
+      .then((res) => {
+        const data = res.data;
+        const userData = {
+          isLogin: data.data.isLogin,
+          username: data.data.username,
+          role: data.data.role,
+        };
+        setUser(userData);
+      })
+      .catch(() => {
+        setUser(null);
+      })
+      .finally(() => setLoading(false));
+  }, [setUser]);
+
+  if (loading) return <div></div>;
+
   return (
     <BrowserRouter>
       <Routes>
-        {/* frontend route */}
         <Route path="/" element={<FrontendLayout />}>
-          <Route path="" element={<HomePage />} />
+          <Route index element={<HomePage />} />
           <Route path="home" element={<HomePage />} />
           <Route path="*" element={<NotFoundPage />} />
         </Route>
-        {/* backend route */}
-        <Route path="/dashboard" element={<DashboardLayout />}>
-          <Route path="" element={<HomePageDash />} />
-          <Route path="pos" element={<POS />} />
-          <Route path="home" element={<HomePageDash />} />
-          <Route path="product/category" element={<CategoryPageDash />} />
-          <Route path="product/productList" element={<ProductPageDash />} />
-          <Route path="user/employee" element={<EmployeePageDash />} />
-          <Route path="user/customer" element={<CustomerPageDash />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Route>
-        {/* Login Route */}
+
         <Route path="/auth" element={<LayoutAuth />}>
           <Route path="signin" element={<SignInPage />} />
           <Route path="signup" element={<SignupPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Route>
+
+        <Route path="/dashboard" element={<ProtectedRoute user={user} />}>
+          <Route path="" element={<DashboardLayout />}>
+            <Route index element={<HomePageDash />} />
+            <Route path="pos" element={<POS />} />
+            <Route path="home" element={<HomePageDash />} />
+            <Route path="product/category" element={<CategoryPageDash />} />
+            <Route path="product/productList" element={<ProductPageDash />} />
+            <Route path="user/employee" element={<EmployeePageDash />} />
+            <Route path="user/customer" element={<CustomerPageDash />} />
+          </Route>
           <Route path="*" element={<NotFoundPage />} />
         </Route>
       </Routes>
